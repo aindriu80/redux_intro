fa22c8c2ed04a171787739a8c611c2f868afd6d8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-04 16:18:32 +0100

    Working with actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { INCREMENT } from "./actions";
+import { Component } from "@angular/core";
+import { NgRedux } from "ng2-redux";
+import { IAppState } from "./store";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
+  counter = 0;
+
+  constructor(private ngRedux: NgRedux<IAppState>) {}
+
+  increment() {
+    // this.counter++;
+    this.ngRedux.dispatch({ type: INCREMENT });
+  }
 }
